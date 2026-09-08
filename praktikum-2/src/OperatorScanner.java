cef1a28167b5d4b65797db560856789baa87e114
Nadya Assyifa <nadyaassyifanad@gmail.com>
2026-09-08 11:37:00 +0700

    Latihan no.5

---
+++ praktikum-2/src/OperatorScanner.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class OperatorScanner {
+    public  static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan bilangan pertama: ");
+        int a = input.nextInt();
+
+        System.out.print("Masukkan bilangan kedua: ");
+        int b = input.nextInt();
+
+        System.out.println("\nHasil Operator Aritmatika:");
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a - b));
+        System.out.println("a * b = " + (a * b));
+        System.out.println("a / b = " + (a / b));
+        System.out.println("a % b = " + (a % b));
+
+        System.out.println("\nHasil Perbandingn:");
+        System.out.println("a > b = " + (a > b));
+        System.out.println("a < b = " + (a < b));
+        System.out.println("a == b = " + (a == b));
+    }
+}
