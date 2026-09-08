c4730526ad7871effe986b967bd649081f23cb5b
Nadya Assyifa <nadyaassyifanad@gmail.com>
2026-09-08 08:45:48 +0700

    Struktur Dasar Program Java

--- praktikum-2/src/Helloworld.java
+++ praktikum-2/src/Helloworld.java
@@ -1,5 +1,9 @@
 public class Helloworld {
     public static void main(String[] args) {
         System.out.println("Nadya Assyifa");
+        System.out.println("Nama: ");
+        System.out.println("Budi");
+        System.out.println();
+        System.out.println("Ini baris baru");
     }
 }
