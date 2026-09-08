4b7c3dac1d3f57598266f21e231e8b23afcd0932
Nadya Assyifa <nadyaassyifanad@gmail.com>
2026-09-08 11:08:55 +0700

    Latihan no.2

--- praktikum-2/src/BiodataSaya.java
+++ praktikum-2/src/BiodataSaya.java
@@ -1,10 +1,11 @@
 public class BiodataSaya {
     public static void main(String[] args) {
         // Menampilkan nama mahasiswa
-        System.out.println("Nama: Nadya Assyifa");
+        System.out.print("Nama: Nadya Assyifa - ");
+        System.out.print("2025573010057");
 
         // Menampilkan NIM dan program studi
-        System.out.println("NIM: 2025573010057");
+        System.out.println();
         System.out.println("Program Studi: Teknik Informatika");
     }
 }
