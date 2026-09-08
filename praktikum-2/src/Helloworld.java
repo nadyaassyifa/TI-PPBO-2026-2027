8488d9d80a41fea9a65efcb0c83e051f3bfbe565
Nadya Assyifa <nadyaassyifanad@gmail.com>
2026-09-08 08:56:49 +0700

    Komentar Kode

--- praktikum-2/src/Helloworld.java
+++ praktikum-2/src/Helloworld.java
@@ -1,9 +1,9 @@
 public class Helloworld {
     public static void main(String[] args) {
-        System.out.println("Nadya Assyifa");
-        System.out.println("Nama: ");
-        System.out.println("Budi");
-        System.out.println();
-        System.out.println("Ini baris baru");
+        // Program menampilkan biodata sederana
+        System.out.println("Nama: Budi ");
+        /* Baris di bawah ini
+           mencetak NIM mahasiswa */
+        System.out.println("NIM: 12345");
     }
 }
